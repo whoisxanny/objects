4ce0c2e27e589f5f119edf3c96cbd650eea43272
whoisxanny <tuffx4nny@gmail.com>
2023-10-21 09:22:08 +0500

    lessons changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
+        Person sarah = new Person("Sarah",30);
+        System.out.println("sarah.name = " + sarah.getName());
+        System.out.println("sarah.age = " + sarah.getAge());
+        Person john = new Person("John", 13);
+        System.out.println("john.age = " + john.getAge());
+        sarah.setAge(31);
+        System.out.println("sarah.getAge = " + sarah.getAge());
     }
 }
